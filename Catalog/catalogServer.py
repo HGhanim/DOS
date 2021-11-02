c59c78f62b2eb1d16214f88a967e85c22dbdf79a
Heba Ghanim <hghanim@asaltech.com>
2021-11-02 20:52:17 +0200

    add search route

--- Catalog/catalogServer.py
+++ Catalog/catalogServer.py
@@ -1,7 +1,15 @@
 from flask import Flask
+import pandas as pd
 
 catalog_server = Flask(__name__)
 
+@catalog_server.route('/search/<string:subject>')
+def search(subject):
+    catalog = pd.read_csv('catalog_file.csv')
+    data = catalog.loc[catalog['subject'] == subject]
+    dataFrame = pd.DataFrame(data, columns = ['id','title'])
+    json = dataFrame.to_json(orient="records")
+    return {'data': json}
 
 def main():
     catalog_server.run(debug=True)
