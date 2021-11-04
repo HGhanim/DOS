087fb4a7a8c62f0c536b5173c0672ed494b0281b
Heba Ghanim <hghanim@asaltech.com>
2021-11-04 20:58:09 +0200

    add purchase route

--- Order/orderServer.py
+++ Order/orderServer.py
@@ -1,7 +1,24 @@
 from flask import Flask
 import pandas as pd
+import requests
+import json
 
 order_server = Flask(__name__)
+
+@order_server.route('/purchase/<int:id>')
+def purchase(id):
+    info = requests.get('http://192.168.1.207:5000/info/'+str(id)).json()
+    book = json.loads(info['data'])[0]
+    if(book['quantity'] > 0):
+        order = {
+            'id' : id,
+           'title'   : [book['title']],
+           'price'   : [book['price']]
+           }
+        orderDataFrame = pd.DataFrame(order)
+        orderDataFrame.to_csv('orders_file.csv', mode='a', index=False, header=False)
+
+        return {"info":"hi"}
 
 def main():
     order_server.run(debug=True)
